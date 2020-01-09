1c99df53c0618ea3b1eb44f713d5626b823db428
Aslam Rasheed <aslampr07@outlook.com>
2020-01-09 21:38:12 +0530

    Creates basic UI structure

---
+++ app/src/main/java/com/kadbyte/client/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.kadbyte.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
