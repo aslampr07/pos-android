48350b53ea6e3abc242f779b28c8d04991f1bf6a
Aslam Rasheed <aslampr07@outlook.com>
2020-02-15 11:23:29 +0530

    Add empty activity for item adding

---
+++ app/src/main/java/com/kadbyte/client/ui/ItemAddActivity.kt
@@ -0,0 +1,13 @@
+package com.kadbyte.client.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kadbyte.client.R
+
+class ItemAddActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_item_add)
+    }
+}
